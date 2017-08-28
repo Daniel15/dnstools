71b772ebe3de37ceebbad974d5b2225a162c10c5
Daniel Lo Nigro <daniel@dan.cx>
2017-08-27 22:21:04 -0700

    Add Google Analytics

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,14 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
         integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"/>
   <link rel="stylesheet" type="text/css" href="/res/style.css"/>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-25623237-9', 'auto');
+    ga('send', 'pageview');
+  </script>
 </head>
 <body>
 <div class="container">
