43d2f2a985bd99d71f843da590c0e80976fc236d
Daniel Lo Nigro <daniel@dan.cx>
2017-08-27 19:21:19 -0700

    Fancier whois page

--- public/whois.php
+++ public/whois.php
@@ -40,15 +40,149 @@ echo '<form method="post" action="/whois">';
 require '../includes/whois_form.php';
 echo '</form>';
 
+function format_contact(\Novutec\WhoisParser\Result\Contact $contact) {
+	$output = '';
+	if (!empty($contact->name)) {
+		$output .= '<strong>'.htmlspecialchars($contact->name)."</strong>\n";
+	}
+  if (!empty($contact->organization)) {
+    $output .= htmlspecialchars($contact->organization)."\n";
+  }
+  if (!empty($contact->email)) {
+    $output .= 'Email: '.htmlspecialchars($contact->email)."\n";
+  }
+  if (!empty($contact->phone)) {
+    $output .= 'Phone: '.htmlspecialchars($contact->phone)."\n";
+  }
+  if (!empty($contact->fax)) {
+    $output .= 'Fax: '.htmlspecialchars($contact->fax)."\n";
+  }
+
+	$address = '';
+  if (!empty($contact->address)) {
+    $address .= htmlspecialchars($contact->address).', ';
+  }
+  if (!empty($contact->city)) {
+    $address .= htmlspecialchars($contact->city).' ';
+  }
+  if (!empty($contact->state)) {
+    $address .= htmlspecialchars($contact->state).' ';
+  }
+  if (!empty($contact->zipcode)) {
+    $address .= htmlspecialchars($contact->zipcode).' ';
+  }
+  if (!empty($contact->country)) {
+    $address .= htmlspecialchars($contact->country).' ';
+  }
+
+  if ($address !== '') {
+  	$output .= 'Address: '.$address."\n";
+	}
+
+	return nl2br($output);
+}
+
 if ($errors !== null) {
 	echo $errors;
 } else {
-	echo '
-	<pre>';
-	// Run the whois.
-	system('whois -H ' . $_POST['host']);
-	echo '
-	</pre>';
+	try {
+    $parser = new Novutec\WhoisParser\Parser();
+    $result = $parser->lookup($_POST['host']);
+		?>
+		<table class="table table-striped mt-4">
+			<tbody>
+				<?php
+				if (!empty($result->created)) {
+					echo '
+						<tr>
+							<th>Created</th>
+							<td>', htmlspecialchars($result->created), '</td>
+						</tr>';
+				}
+        if (!empty($result->changed)) {
+          echo '
+						<tr>
+							<th>Last Changed</th>
+							<td>', htmlspecialchars($result->changed), '</td>
+						</tr>';
+        }
+        if (!empty($result->expires)) {
+          echo '
+						<tr>
+							<th>Expires</th>
+							<td>', htmlspecialchars($result->expires), '</td>
+						</tr>';
+        }
+        if (!empty($result->registrar)) {
+          echo '
+						<tr>
+							<th>Registrar</th>
+							<td>';
+
+					$name = '';
+          if (!empty($result->registrar->name)) {
+          	$name = $result->registrar->name;
+          }
+
+          if (!empty($result->registrar->url)) {
+          	$name = $name === ''
+							? $result->registrar->url
+							: '<a href="'.htmlspecialchars($result->registrar->url).'" rel="nofollow">'.$name.'</a>';
+					}
+					echo $name;
+
+          if (!empty($result->registrar->email)) {
+						echo ', ', htmlspecialchars($result->registrar->email);
+          }
+
+          if (!empty($result->registrar->phone)) {
+            echo ', ', htmlspecialchars($result->registrar->phone);
+          }
+
+					echo '
+							</td>
+						</tr>';
+        }
+        if (!empty($result->nameserver)) {
+          echo '
+						<tr>
+							<th>Nameservers</th>
+							<td>', htmlspecialchars(implode(', ', $result->nameserver)), '</td>
+						</tr>';
+        }
+        if (!empty($result->contacts->owner) && count($result->contacts->owner) !== 0) {
+          echo '
+						<tr>
+							<th>Owner</th>
+							<td>', format_contact($result->contacts->owner[0]), '</td>
+						</tr>';
+        }
+        if (!empty($result->contacts->admin) && count($result->contacts->admin) !== 0) {
+          echo '
+						<tr>
+							<th>Administrative Contact</th>
+							<td>', format_contact($result->contacts->admin[0]), '</td>
+						</tr>';
+        }
+        if (!empty($result->contacts->tech) && count($result->contacts->tech) !== 0) {
+          echo '
+						<tr>
+							<th>Technical Contact</th>
+							<td>', format_contact($result->contacts->tech[0]), '</td>
+						</tr>';
+        }
+				?>
+			</tbody>
+		</table>
+		<h5>Raw Response</h5>
+		<?= '<pre>'.htmlspecialchars($result->rawdata[count($result->rawdata) - 1]).'</pre>'; ?>
+		<?php
+  } catch (Exception $ex) {
+		// PHP library failed, so just fall back to system whois command
+    echo '<pre>';
+    system('whois -H ' . $_POST['host']);
+    echo '</pre>';
+	}
 }
 
 include '../includes/footer.php';
