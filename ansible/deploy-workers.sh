c44f0ffda247a5a083cba1ff14b704054541c88e
Daniel Lo Nigro <d@d.sb>
2019-12-04 23:51:14 -0800

    Add Ansible playbook for worker deployment

---
+++ ansible/deploy-workers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ex
+if [ ! -d ../src/DnsTools.Worker/bin/Release/netcoreapp3.0/linux-x64/publish/ ]; then
+	dotnet.exe publish ../src/DnsTools.Worker --no-self-contained -r linux-x64 -c Release
+fi
+ansible-playbook worker.yml --extra-vars '@passwd.yml' --vault-password-file=vault-password
