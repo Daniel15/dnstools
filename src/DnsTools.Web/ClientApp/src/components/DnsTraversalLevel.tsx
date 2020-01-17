d128db65664db02ef814b1871ab2261066a2a3f4
Daniel Lo Nigro <d@d.sb>
2020-01-16 21:38:10 -0800

    [WIP] Simplify UI for DNS traversal when all servers return the same result

--- src/DnsTools.Web/ClientApp/src/components/DnsTraversalLevel.tsx
+++ src/DnsTools.Web/ClientApp/src/components/DnsTraversalLevel.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import {DnsTraversalResponse} from '../types/protobuf';
+import {DnsTraversalResponse, DnsRecord} from '../types/protobuf';
 import DnsRecordsSummaryTable from '../components/DnsRecordsSummaryTable';
-import {DnsLookupType} from '../types/generated';
+import {DnsLookupType, DnsTraversalResponseType} from '../types/generated';
 
 type Props = Readonly<{
   lookupType: DnsLookupType;
@@ -12,17 +12,67 @@ type Props = Readonly<{
 export default function DnsTraversalLevel(props: Props) {
   const serversLeft = props.servers.size - props.responses.length;
 
+  const areSame = areAllResponsesSame(props.responses);
+
   // TODO: Don't show table if all records are the same across all servers
   // (just show the records once, and "all servers returned the same response")
   return (
     <div>
       Looking on {props.servers.size} servers...{' '}
       {serversLeft > 0 && <>{serversLeft} remaining</>}
-      <DnsRecordsSummaryTable
-        lookupType={props.lookupType}
-        responses={props.responses}
-        serversToShow={props.servers}
-      />
+      {props.responses.length > 0 && (
+        <>
+          {serversLeft === 0 && areSame && <>All returned the same response</>}
+        </>
+      )}
+      {props.responses.length > 0 && !areSame && (
+        <DnsRecordsSummaryTable
+          lookupType={props.lookupType}
+          responses={props.responses}
+          serversToShow={props.servers}
+        />
+      )}
     </div>
   );
+}
+
+function areAllResponsesSame(
+  responses: ReadonlyArray<DnsTraversalResponse>,
+): boolean {
+  if (responses.length === 0) {
+    return false;
+  }
+
+  const firstResponse = responses[0];
+  if (firstResponse.responseCase !== DnsTraversalResponseType.Reply) {
+    return false;
+  }
+
+  for (let i = 0; i < responses.length; i++) {
+    const response = responses[i];
+    if (response.responseCase !== DnsTraversalResponseType.Reply) {
+      return false;
+    }
+    if (
+      !areAllRecordsSame(
+        firstResponse.reply.answers || firstResponse.reply.authorities,
+        response.reply.answers || response.reply.authorities,
+      )
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function areAllRecordsSame(
+  first: ReadonlyArray<DnsRecord>,
+  second: ReadonlyArray<DnsRecord>,
+): boolean {
+  if (first.length !== second.length) {
+    return false;
+  }
+  // This should probably be a smarter deep comparison, but this will do for now.
+  return JSON.stringify(first) === JSON.stringify(second);
 }
