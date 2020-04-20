540b8cb0d7ebdebe0281c7f6b4de18f65b1d7055
Daniel Lo Nigro <d@d.sb>
2020-04-19 17:48:24 -0700

    Use ReactDOM.hydrate for prerendered content

--- src/DnsTools.Web/ClientApp/src/index.tsx
+++ src/DnsTools.Web/ClientApp/src/index.tsx
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root')!;
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement);
+} else {
+  ReactDOM.render(<App />, rootElement);
+}
