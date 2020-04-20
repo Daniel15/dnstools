50f18e518db74c504311c7172bf3d1c86fdd9579
Daniel Lo Nigro <d@d.sb>
2020-04-19 17:12:50 -0700

    Move site publish script from csproj into separate script

---
+++ src/DnsTools.Web/publish.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = "Stop"
+
+# Checks that the last ran command returned with an exit code of 0
+function Assert-LastExitCode {
+	if ($LASTEXITCODE -ne 0) {
+	  throw 'Non-zero exit code encountered'
+	}
+  }
+
+cd ClientApp
+yarn install --frozen-lockfile; Assert-LastExitCode
+yarn run build; Assert-LastExitCode
+yarn run generate-cshtml; Assert-LastExitCode
+cd ..
+
+cd legacy
+composer install --apcu-autoloader; Assert-LastExitCode
+cd ..
+
+dotnet publish --no-self-contained -r linux-x64 -c Release; Assert-LastExitCode
