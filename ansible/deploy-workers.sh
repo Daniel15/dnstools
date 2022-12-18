57de9a42331df18c4fae1f93888b08e28dc5a9f3
Daniel Lo Nigro <d@d.sb>
2022-12-17 19:49:35 -0800

    Ensure correct config is used when running Ansible playbook

--- ansible/deploy-workers.sh
+++ ansible/deploy-workers.sh
@@ -3,4 +3,4 @@ set -ex
 if [ ! -d ../src/DnsTools.Worker/bin/Release/net5.0/linux-x64/publish/ ]; then
 	dotnet.exe publish ../src/DnsTools.Worker --no-self-contained -r linux-x64 -c Release
 fi
-ansible-playbook worker.yml --extra-vars '@passwd.yml' --vault-password-file=vault-password
+ANSIBLE_CONFIG=/mnt/c/src/dnstools.ws/ansible/ansible.cfg ansible-playbook worker.yml --extra-vars '@passwd.yml' --vault-password-file=vault-password
