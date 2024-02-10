6fbd6a1c352a89bf41d22c48bede82243d54725d
Daniel Lo Nigro <d@d.sb>
2024-02-10 15:39:52 -0800

    Update Ansible to deploy .NET 8 AoT build

--- ansible/deploy-workers.sh
+++ ansible/deploy-workers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
-if [ ! -d ../src/DnsTools.Worker/bin/Release/net7.0/linux-x64/publish/ ]; then
-	dotnet.exe publish ../src/DnsTools.Worker --no-self-contained -r linux-x64 -c Release
+if [ ! -d ../src/DnsTools.Worker/bin/Release/net8.0/linux-x64/publish/ ]; then
+	dotnet publish ../src/DnsTools.Worker -r linux-x64 -c Release
 fi
+
 ANSIBLE_CONFIG=/mnt/c/src/dnstools.ws/ansible/ansible.cfg ansible-playbook worker.yml --extra-vars '@passwd.yml' --vault-password-file=vault-password
